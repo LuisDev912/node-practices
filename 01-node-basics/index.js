138322e83ce8044b6869ced9f4f6421b9a6ba51e
Luis <luiscallejaenrique@gmail.com>
2026-01-11 14:51:23 -0300

    "01-node-basics: fix method calls and format memory - Include '()' when calling an os method. - Create a formatMem() function to format the number that os.freemem() and os.totalmem() give."

--- 01-node-basics/index.js
+++ 01-node-basics/index.js
@@ -5,13 +5,20 @@ import os from 'node:os';
 
 // code
 
+function formatMem(memory){
+    const gb = memory / 1024 / 1024 / 1024;
+    return `${gb.toFixed(2)} gb`
+}
+
 console.group(' --- OS information: --- ');
-console.log(`Type of OS: ${os.type()}${os.arch}`);
+console.log(`Type of OS: ${os.type()} (${os.arch()})`);
 console.log(`Type of platform: ${os.platform()}`);
-console.log(`Total memory: ${os.totalmem}. And free memory: ${os.freemem}`);
+
+console.log(`Total memory: ${formatMem(os.totalmem())}. And free memory: ${formatMem(os.freemem())}`);
+
 console.log(`System active time: ${os.uptime()}`);
-console.log(`Your home dir is: ${os.homedir}`);
-console.log(`Your PC current version is: ${os.version}`);
+console.log(`Your home dir is: ${os.homedir()}`);
+console.log(`Your PC current version is: ${os.version()}`);
 console.groupEnd();
 
 // other methods: os.endianness, os.EOL, etc.
