eab38926281ea6978cc0b2255b5acaa8c76ededa
Luis <luiscallejaenrique@gmail.com>
2026-01-11 15:26:23 -0300

    01-node-basics: add other information that includes execution mode, port, etc.

--- 01-node-basics/index.js
+++ 01-node-basics/index.js
@@ -1,10 +1,15 @@
 /* System & Runtime Inspector */
 
-// imports
+/* imports */
 import os from 'node:os';
 
-// code
+/* code */
 
+// --- variables
+const mode = process.argv[2] ?? 'default';
+const port = process.env.PORT ?? 'not defined';
+
+// --- functions
 function formatMem(memory){
     const gb = memory / 1024 / 1024 / 1024;
     return `${gb.toFixed(2)} gb`
@@ -18,6 +23,7 @@ function formatTime(time){
     return `${h}h ${m}m ${s}s`
 }; // this can also be done with the 'ms' library from Vercel
 
+// --- logs
 console.group(' --- OS information: --- ');
 console.log(`Type of OS: ${os.type()} (${os.arch()})`);
 console.log(`Type of platform: ${os.platform()}`);
@@ -28,6 +34,15 @@ console.log(`System active time: ${formatTime(os.uptime())}`);
 
 console.log(`Your home dir is: ${os.homedir()}`);
 console.log(`Your PC current version is: ${os.version()}`);
+console.groupEnd();
+
+// -------------
+
+console.group(' --- Other information: ---');
+console.log(`Execution mode: ${mode}`);
+console.log(`PORT: ${port}`);
+console.log(`Node uptime: ${process.uptime().toFixed(2)}`)
+
 console.groupEnd();
 
 // other methods: os.endianness, os.EOL, etc.
