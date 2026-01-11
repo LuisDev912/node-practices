9fda4a65bd5ee6dc54f0f7967329430832135152
Luis <luiscallejaenrique@gmail.com>
2026-01-11 14:40:57 -0300

    01-node-basics: improve the information of the machine

--- 01-node-basics/index.js
+++ 01-node-basics/index.js
@@ -5,10 +5,13 @@ import os from 'node:os';
 
 // code
 
-console.group(' --- OS information: --- ')
-console.log(`Type of OS: ${os.type()}`)
-console.log(`Type of platform: ${os.platform()}`)
-console.log(`Total memory: ${os.totalmem}. And free memory: ${os.freemem}`)
-console.log(`System active time: ${os.uptime()}`)
-
+console.group(' --- OS information: --- ');
+console.log(`Type of OS: ${os.type()}${os.arch}`);
+console.log(`Type of platform: ${os.platform()}`);
+console.log(`Total memory: ${os.totalmem}. And free memory: ${os.freemem}`);
+console.log(`System active time: ${os.uptime()}`);
+console.log(`Your home dir is: ${os.homedir}`);
+console.log(`Your PC current version is: ${os.version}`);
 console.groupEnd();
+
+// other methods: os.endianness, os.EOL, etc.
