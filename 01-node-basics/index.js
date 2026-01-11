80ca33098ca5e9008992b0f1a3ef6bd1b3d0fb81
Luis <luiscallejaenrique@gmail.com>
2026-01-11 14:31:15 -0300

    01-node-basics: add more logs with information of the system

--- 01-node-basics/index.js
+++ 01-node-basics/index.js
@@ -5,5 +5,10 @@ import os from 'node:os';
 
 // code
 
-console.group('OS information: ')
-console.log()
+console.group(' --- OS information: --- ')
+console.log(`Type of OS: ${os.type()}`)
+console.log(`Type of platform: ${os.platform()}`)
+console.log(`Total memory: ${os.totalmem}. And free memory: ${os.freemem}`)
+console.log(`System active time: ${os.uptime()}`)
+
+console.groupEnd();
