df7f759f3db28c64cf6d0f6fce9cb322beb40d6b
Luis <luiscallejaenrique@gmail.com>
2026-01-12 16:18:43 -0300

    feat(02-fs-basics): create a function to show the information of the file

--- 02-fs-basics/index.js
+++ 02-fs-basics/index.js
@@ -8,21 +8,29 @@ import { join, extname, basename } from "node:path";
 
 // --- variables
 const mode = process.argv[2] ?? 'example.txt';
-let content = ''
+let content;
 
-content = await readFile(mode, 'utf-8')
+content = await readFile(mode, 'utf-8');
+console.log(content);
 
 // --- functions
 
-async function createDir(file){
+function showFileInformation(file){
+    console.log(`your file name is: ${basename(file)}`);
+    console.log(`Your file extension is: ${extname(file)}`)
+}
+
+async function createDir(){
     const outputDir = join('output', 'files');
     await mkdir(outputDir, { recursive: true });
 
-    const uppercaseContent = content.toUpperCase()
-    const outputFilePath = join(outputDir, 'uppercase-file.txt')
+    const uppercaseContent = content.toUpperCase();
+    const outputFilePath = join(outputDir, 'uppercase-file.txt');
 
-    await writeFile(outputFilePath, uppercaseContent)
-    console.log(`File in uppercase created successfully. Check ${outputDir} to see your file`)
+    await writeFile(outputFilePath, uppercaseContent);
+
+    console.log(`File in uppercase created successfully. Check ${outputDir} to see your file`);
+    showFileInformation(outputFilePath)
 }
 
 createDir(content);
