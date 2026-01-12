ef875ee734fa738c68e5c6644c445c1f91ced45c
Luis <luiscallejaenrique@gmail.com>
2026-01-12 15:44:19 -0300

    feat(02-fs-basics): create a function that creates two folders

--- 02-fs-basics/index.js
+++ 02-fs-basics/index.js
@@ -2,12 +2,22 @@
 
 /* imports */
 import { readFile, mkdir, writeFile } from "node:fs/promises";
+import { join, extname, basename } from "node:path";
 
 /* code */
 
 // --- variables
+const mode = process.argv[2] ?? 'example.txt';
 let content = ''
 
-content = await readFile('example.txt', 'utf-8')
+content = await readFile(mode, 'utf-8')
+
+// --- functions
+
+async function createDir(){
+    const outputDir = join('output', 'files');
+    await mkdir(outputDir, { recursive: true })
+}
 
 console.log(content);
+createDir();
