50bac0a22dc145d3028c06ad4fc99b9562f86468
Luis <luiscallejaenrique@gmail.com>
2026-01-14 17:32:43 -0300

    "refactor(03-path-and-cli): improve code readability - Remove unnecessary join() method. - Nest the logs inside a console.group(). - Show messages if the user selected a path or if the user did not select a file."

--- 03-path-and-cli/index.js
+++ 03-path-and-cli/index.js
@@ -1,16 +1,29 @@
 /* Path Inspector CLI */ 
 
 /* imports */
-import { join, extname, basename, resolve, dirname } from "node:path";
+import { extname, basename, resolve, dirname } from "node:path";
 
 /* code */
 
 // --- variables
-const relativePath = process.argv[2] ?? '.';
+const relativePath = process.argv[2] ?? './';
 const absolutePath = resolve(relativePath);
+let extensionName;
 
-console.log(`selected path: ${join(relativePath)}`)
-console.log(`resolved path: ${join(absolutePath)}`)
-console.log(`base name: ${basename(absolutePath)}`)
-console.log(`extension name: ${extname(absolutePath)}`);
-console.log(`parent path: ${dirname(absolutePath)}`)
+if(!process.argv[2]){
+    console.log('No path or file selected. Using current directory (./)');
+};
+
+if(!extname(relativePath)){
+    extensionName = 'no file selected';
+} else{
+    extensionName = extname(absolutePath);
+}
+
+console.group('--- INFORMATION ABOUT THE SELECTED PATH --- \n');
+console.log(`selected path: ${relativePath}`);
+console.log(`resolved path: ${absolutePath}`);
+console.log(`base name: ${basename(absolutePath)}`);
+console.log(`extension name: ${extensionName}`);
+console.log(`parent path: ${dirname(absolutePath)}`);
+console.groupEnd();
