5b5d66a588987c530976382772a5f1e27e970144
Luis <luiscallejaenrique@gmail.com>
2026-01-14 17:43:07 -0300

    refactor(03-path-and-cli): extract extension detection logic into a function

--- 03-path-and-cli/index.js
+++ 03-path-and-cli/index.js
@@ -8,22 +8,20 @@ import { extname, basename, resolve, dirname } from "node:path";
 // --- variables
 const relativePath = process.argv[2] ?? './';
 const absolutePath = resolve(relativePath);
-let extensionName;
 
 if(!process.argv[2]){
     console.log('No path or file selected. Using current directory (./)');
 };
 
-if(!extname(relativePath)){
-    extensionName = 'no file selected';
-} else{
-    extensionName = extname(absolutePath);
+function getExtensionInfo(path){
+    const ext = extname(path);
+    return ext ? ext : 'no extension detected'
 }
 
 console.group('--- INFORMATION ABOUT THE SELECTED PATH --- \n');
 console.log(`selected path: ${relativePath}`);
 console.log(`resolved path: ${absolutePath}`);
 console.log(`base name: ${basename(absolutePath)}`);
-console.log(`extension name: ${extensionName}`);
+console.log(`extension name: ${getExtensionInfo(absolutePath)}`);
 console.log(`parent path: ${dirname(absolutePath)}`);
 console.groupEnd();
