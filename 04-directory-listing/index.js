8fb79726a3842b0ad413b4d9f1d7493149efd74a
Luis <luiscallejaenrique@gmail.com>
2026-01-15 10:25:26 -0300

    "feat(04-directory-listing): create a main() function with try/catch - Move a great part of the code to the try/catch statement to handle errors. - Add a validation block to validate if the path is a file."

--- 04-directory-listing/index.js
+++ 04-directory-listing/index.js
@@ -8,10 +8,33 @@ import { readdir, stat } from 'node:fs/promises';
 
 // --- variables
 const path = process.argv[2] ?? './';
-console.log(`path selected: ${path}`);
+const files = await readdir(path);
 
 // --- functions
 function formatMem(memory){
     const MB = memory / 1024 / 1024;
     return `${MB.toFixed(2)} MB`
 }
+
+async function main(){
+    
+    try {
+        if(!process.argv[2]){
+            console.log('No path or file selected. Using current directory (./)');
+        };
+
+        const stats = await stat(path);
+
+        console.log(`path selected: ${path}, its files: ${files}`);
+        
+        if (stats.isFile()) {
+            console.log(`the size of the file is: ${formatMem(stats.size)}`);
+        }
+
+    } catch (e) {
+        console.log(`An error has ocurred: ${e.message}`);
+        process.exit(1)
+    }
+}
+
+main()
