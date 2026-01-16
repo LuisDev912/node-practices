f55aa7ec42c8bed98561a2cc6f2bd865b7507f6b
Luis Calleja <luiscallejaenrique@gmail.com>
2026-01-16 14:39:31 -0300

    refactor(04-directory-listing): divide logic into new functions

--- 04-directory-listing/index.js
+++ 04-directory-listing/index.js
@@ -21,31 +21,38 @@ function formatMem(bytes) {
     return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i]
 }
 
+function render(entries) {
+    for (const entry of entries) {
+        const icon = entry.isDir ? '📁' : '📄';
+        const size = entry.isDir ? '-' : `${entry.size}`
+
+        console.log(`${icon}  ${entry.name}  ${size}`)
+    };
+}
+
+async function dirEntries() {
+    const entries = await Promise.all(
+        files.map(async (name) => {
+            const fullPath = join(path, name);
+            const stats = await stat(fullPath);
+
+            return {
+                name,
+                isDir: stats.isDirectory(),
+                size: formatMem(stats.size)
+            }
+        })
+    );
+
+    render(entries)
+}
+
 async function main(){
     try {
         if(!process.argv[2]){
             console.log('No path or file selected. Using current directory (./)');
         };
-
-        const entries = await Promise.all(
-            files.map(async (name) => {
-                const fullPath = join(path, name);
-                const stats = await stat(fullPath);
-
-                return {
-                    name,
-                    isDir: stats.isDirectory(),
-                    size: formatMem(stats.size)
-                }
-            })
-        );
-
-        for (const entry of entries) {
-            const icon = entry.isDir ? '📁' : '📄';
-            const size = entry.isDir ? '-' : `${entry.size}`
-
-            console.log(`${icon}  ${entry.name}  ${size}`)
-        };
+        dirEntries();
     } catch (e) {
         console.log(`An error has occurred: ${e.message}`);
         process.exit(1);
