b6cc808e3318b4f77c88d931d0e6eccdb8952f13
Luis Calleja <luiscallejaenrique@gmail.com>
2026-01-17 13:45:17 -0300

    feat(06-routing-basics): create a getHome() function for the home url

--- 06-routing-basics/handlers.js
+++ 06-routing-basics/handlers.js
@@ -1,5 +1,12 @@
 // these functions only answer when called. They do not decide routes
 
+export function getHome(_, res) {
+    res.writeHead(200, {
+        "Content-Type": "text/plain; charset=utf-8"
+    });
+    res.end('Hello world from node');
+}
+
 export function notFound(_, res) {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
--- 06-routing-basics/routes.js
+++ 06-routing-basics/routes.js
@@ -1,5 +1,8 @@
+import { getHome } from "./handlers.js";
+
 export const routes = {
     GET: {
         // this contains a map of routes
+        '/': getHome
     }
 };
