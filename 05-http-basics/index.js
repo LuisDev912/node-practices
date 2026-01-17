315378eaac2f82e076ca4fd3f23a6e2216f16bd0
Luis Calleja <luiscallejaenrique@gmail.com>
2026-01-17 09:46:14 -0300

    feat(05-http-basics): create new GET routes - Create "/health" route that returns the code 200 with a JSON. - Create a plainToJSON() function to return JSON text. - Add a 404 status code message if the method and url are not included.

--- 05-http-basics/index.js
+++ 05-http-basics/index.js
@@ -8,9 +8,29 @@ import { createServer } from 'node:http';
 // --- variables
 const port = process.env.PORT ?? 3000;
 
+// --- functions
+function plainToJSON(res, statusCode, data) {
+    res.statusCode = statusCode;
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    res.end(JSON.stringify(data));
+}
+
+// --- server
 const server = createServer(async (req, res) => {
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.end('Hello world from Node');
+    const { method, url } = req;
+
+    if (method === 'GET') {
+        if (url === '/') {
+            res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+            return res.end('Hello world from Node');
+        }
+
+        if (url === '/health') {
+            return plainToJSON(res, 200, { status: 'ok'})
+        }
+    }
+
+    return plainToJSON(res, 404, { status: 'Not found' });
 });
 
 server.listen(port, () => {
